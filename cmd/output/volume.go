7ad0462f889f2c44157ffcf3a7c371dd7551d67d
Stefan Majer <stefan.majer@f-i-ts.de>
2020-12-17 10:34:49 +0100

    add volume pv

--- cmd/output/volume.go
+++ cmd/output/volume.go
@@ -4,7 +4,11 @@ import (
 	"fmt"
 	"strings"
 
+	"github.com/dustin/go-humanize"
 	"github.com/fi-ts/cloud-go/api/models"
+	"gopkg.in/yaml.v3"
+	corev1 "k8s.io/api/core/v1"
+	v1 "k8s.io/apimachinery/pkg/apis/meta/v1"
 )
 
 type (
@@ -26,7 +30,7 @@ func (p VolumeTablePrinter) Print(data []*models.V1VolumeResponse) {
 		}
 		size := ""
 		if vol.Size != nil {
-			size = fmt.Sprintf("%d", *vol.Size)
+			size = fmt.Sprintf("%s", humanize.Bytes(uint64(*vol.Size)))
 		}
 		replica := ""
 		if vol.ReplicaCount != nil {
@@ -48,4 +52,54 @@ func (p VolumeTablePrinter) Print(data []*models.V1VolumeResponse) {
 		p.addShortData(short, vol)
 	}
 	p.render()
+}
+
+/*
+Name:            pvc-e86fe06d-7d5e-44e0-b83b-e6758caa2826
+Labels:          <none>
+Annotations:     pv.kubernetes.io/provisioned-by: csi.lightbitslabs.com
+Finalizers:      [kubernetes.io/pv-protection external-attacher/csi-lightbitslabs-com]
+StorageClass:    partition-storage-gold
+Status:          Bound
+Claim:           default/example-mt-pvc
+Reclaim Policy:  Delete
+Access Modes:    RWO
+VolumeMode:      Filesystem
+Capacity:        10Gi
+Node Affinity:   <none>
+Message:
+Source:
+    Type:              CSI (a Container Storage Interface (CSI) volume source)
+    Driver:            csi.lightbitslabs.com
+    FSType:            ext4
+	VolumeHandle:      mgmt:10.131.44.1:443,10.131.44.2:443,10.131.44.3:443|nguid:9304ff62-2d56-4d67-9c3d-80585a6795df|proj:b5f26a3b-9a4d-48db-a6b3-d1dd4ac4abec|scheme:grpcs
+	                                                                              <volume.UUID>                             <volume.ProjectName>
+    ReadOnly:          false
+    VolumeAttributes:      storage.kubernetes.io/csiProvisionerIdentity=1607936819649-8081-csi.lightbitslabs.com
+Events:                <none>
+*/
+func PersistenVolume(v models.V1VolumeResponse) error {
+	filesystem := corev1.PersistentVolumeFilesystem
+	pv := corev1.PersistentVolume{
+		ObjectMeta: v1.ObjectMeta{Name: "your-name-here", Namespace: "your-namespace-here"},
+		Spec: corev1.PersistentVolumeSpec{
+			AccessModes:      []corev1.PersistentVolumeAccessMode{corev1.ReadWriteOnce},
+			VolumeMode:       &filesystem,
+			StorageClassName: *v.StorageClass,
+			PersistentVolumeSource: corev1.PersistentVolumeSource{
+				CSI: &corev1.CSIPersistentVolumeSource{
+					Driver:       "csi.lightbitslabs.com",
+					FSType:       "ext4",
+					ReadOnly:     false,
+					VolumeHandle: *v.VolumeHandle,
+				},
+			},
+		},
+	}
+	yml, err := yaml.Marshal(pv)
+	if err != nil {
+		return fmt.Errorf("unable to marshal to yaml:%v", err)
+	}
+	fmt.Printf("%s\n", string(yml))
+	return nil
 }
--- cmd/volume.go
+++ cmd/volume.go
@@ -25,14 +25,6 @@ var (
 		},
 		PreRun: bindPFlags,
 	}
-	volumeFindCmd = &cobra.Command{
-		Use:   "find",
-		Short: "find volumes",
-		RunE: func(cmd *cobra.Command, args []string) error {
-			return volumeFind()
-		},
-		PreRun: bindPFlags,
-	}
 	volumeDeleteCmd = &cobra.Command{
 		Use:     "delete <volume>",
 		Aliases: []string{"rm", "destroy", "remove", "delete"},
@@ -42,29 +34,35 @@ var (
 		},
 		PreRun: bindPFlags,
 	}
+	volumePVCmd = &cobra.Command{
+		Use:   "pv <volume>",
+		Short: "create a static persistenvolumeclaim for a volume",
+		RunE: func(cmd *cobra.Command, args []string) error {
+			return volumePV(args)
+		},
+		PreRun: bindPFlags,
+	}
 )
 
 func init() {
 	rootCmd.AddCommand(volumeCmd)
 
 	volumeCmd.AddCommand(volumeListCmd)
-	volumeCmd.AddCommand(volumeFindCmd)
 	volumeCmd.AddCommand(volumeDeleteCmd)
+	volumeCmd.AddCommand(volumePVCmd)
 
-	volumeFindCmd.Flags().StringP("volumeid", "", "", "volumeid to filter [optional]")
-	volumeFindCmd.Flags().StringP("project", "", "", "project to filter [optional]")
-	volumeFindCmd.Flags().StringP("partition", "", "", "partition to filter [optional]")
+	volumeListCmd.Flags().StringP("volumeid", "", "", "volumeid to filter [optional]")
+	volumeListCmd.Flags().StringP("project", "", "", "project to filter [optional]")
+	volumeListCmd.Flags().StringP("partition", "", "", "partition to filter [optional]")
 
-	volumeDeleteCmd.Flags().StringP("volumeid", "", "", "volumeid to delete [optional]")
-
-	err := volumeFindCmd.RegisterFlagCompletionFunc("project", func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+	err := volumeListCmd.RegisterFlagCompletionFunc("project", func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 		return projectListCompletion()
 	})
 	if err != nil {
 		log.Fatal(err.Error())
 	}
 
-	err = volumeFindCmd.RegisterFlagCompletionFunc("partition", func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
+	err = volumeListCmd.RegisterFlagCompletionFunc("partition", func(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
 		return partitionListCompletion()
 	})
 	if err != nil {
@@ -97,7 +95,18 @@ func volumeFind() error {
 func volumeDelete(args []string) error {
 	volumeID := args[0]
 	params := &volume.DeleteVolumeParams{}
-	params.SetVolume(volumeID)
+	params.SetID(volumeID)
+	resp, err := cloud.Volume.DeleteVolume(params, cloud.Auth)
+	if err != nil {
+		return err
+	}
+
+	return printer.Print(resp.Payload)
+}
+func volumePV(args []string) error {
+	volumeID := args[0]
+	params := &volume.DeleteVolumeParams{}
+	params.SetID(volumeID)
 	resp, err := cloud.Volume.DeleteVolume(params, cloud.Auth)
 	if err != nil {
 		return err
