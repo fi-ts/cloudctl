210251fa227d08da0a41ab583d89f5d29cd6d706
Stefan Majer <stefan.majer@f-i-ts.de>
2021-03-17 08:16:44 +0100

    short intro

--- cmd/database-postgres.go
+++ cmd/database-postgres.go
@@ -18,7 +18,53 @@ var (
 	postgresCmd = &cobra.Command{
 		Use:   "postgres",
 		Short: "manage postgres",
-		Long:  "list/find/delete postgress",
+		Long: `
+Create an manage postgres databases.
+
+To create a postgres database you first need to specify/create a backup-config where the database should store the backups.
+This can be done either by auto-create a S3 endpoint or provide all S3 details.
+
+Example Workflow:
+
+1. Display all possible locations where postgres databases can be created:
+
+# cloudctl postgres partition
+NAME            ALLOWEDTENANTS
+dc1             any
+
+2. Create a backup-config with retention count and schedule
+
+# cloudctl postgres backup-config auto-create --name daily-for-one-week --project <your-project-id> --partition dc1 --retention 7 --schedule "* * * * *"
+ID                                      NAME                    PROJECT                                 SCHEDULE        RETENTION       S3                                                              CREATEDBY
+3094421c-ee11-4155-b4d9-7fdac116c0ff    daily-for-one-week      b621eb99-4888-4911-93fc-95854fc030e8    * * * * *       7               https://s3.test-01-fra-equ01.metal-pod.dev/backup-3094421c      <Achim Muster>[achim.muster@example.com]
+
+3. Create a postgres database
+
+# cloudctl postgres create --description accounting-db-test --project <your-project-id> --partition dc1 --backup <backup-id-from-above>
+ID                                      DESCRIPTION             PARTITION       TENANT  PROJECT                                 CPU     BUFFER  STORAGE BACKUP                                  REPLICA VERSION AGE     STATUS
+890b1601-6cc3-46cd-86a6-d4479bc1528d    accounting-db-test      nbg-w8101       fits    b621eb99-4888-4911-93fc-95854fc030e8    500m    500m    10Gi    3094421c-ee11-4155-b4d9-7fdac116c0ff    1       12      0s
+
+4. Check if it is running with
+
+# cloudctl postgres ls --description accounting-db-test
+ID                                      DESCRIPTION             PARTITION       TENANT  PROJECT                                 CPU     BUFFER  STORAGE BACKUP                                  REPLICA VERSION AGE     STATUS
+890b1601-6cc3-46cd-86a6-d4479bc1528d    accounting-db-test      nbg-w8101       fits    b621eb99-4888-4911-93fc-95854fc030e8    500m    500m    10Gi    3094421c-ee11-4155-b4d9-7fdac116c0ff    1       12      1m 21s  Running
+
+5. Connect to the database
+
+# cloudctl postgres connectionstring 890b1601-6cc3-46cd-86a6-d4479bc1528d
+PGPASSWORD=J34JnhbtPQ2s1znPmp1pWNRv9EPvbsUvQ3OLh2ycyVAcMmK6upazlzM4JAELpaC0 psql --host=1.2.3.4 --port=32004 --username=postgres
+PGPASSWORD=imQw7KYOdha2wLODlQFFEodN8eyfoFYKOUmXnxFwQJHIeOQtjZJmMGDskpa3SARr psql --host=1.2.3.4 --port=32004 --username=standby
+
+# PGPASSWORD=J34JnhbtPQ2s1znPmp1pWNRv9EPvbsUvQ3OLh2ycyVAcMmK6upazlzM4JAELpaC0 psql --host=1.2.3.4 --port=32004 --username=postgres
+psql (12.6 (Ubuntu 12.6-0ubuntu0.20.10.1))
+SSL connection (protocol: TLSv1.3, cipher: TLS_AES_256_GCM_SHA384, bits: 256, compression: off)
+Type "help" for help.
+
+postgres=#
+
+6. You can create more database, all using the same backup-config
+`,
 	}
 	postgresCreateCmd = &cobra.Command{
 		Use:   "create",
