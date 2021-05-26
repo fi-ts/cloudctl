a62cbeb2c5c6fd31fc9df317c7169e5e08d3f8ab
Lim, Kian-An <lim.kian.an@gmx.de>
2021-05-26 17:13:20 +0200

    Better naming

--- cmd/gateway.go
+++ cmd/gateway.go
@@ -29,43 +29,43 @@ var (
 		Short:   "Manage gateways",
 		Long:    "Manage gateways which enable access to services in another cluster",
 	}
-	gwServerCmd = &cobra.Command{
+	serverCmd = &cobra.Command{
 		Use:   "server",
 		Short: "Manage servers",
 	}
-	gwServerDescribeCmd = &cobra.Command{
-		Use:   "describe <projectUID>--<name>",
-		Long:  "Describe a server",
-		Short: "Describe a server",
-		RunE:  describeGWServer,
-	}
-	gwServerCreateCmd = &cobra.Command{
+	serverCreateCmd = &cobra.Command{
 		Use:    "create <projectUID>--<name>",
 		Long:   "Create a server",
 		Short:  "Create a server",
-		RunE:   createGWServer,
+		RunE:   serverCreate,
 		PreRun: bindPFlags,
 	}
-	gwServerPatchCmd = &cobra.Command{
+	serverDescribeCmd = &cobra.Command{
+		Use:   "describe <projectUID>--<name>",
+		Long:  "Describe a server",
+		Short: "Describe a server",
+		RunE:  serverDescribe,
+	}
+	serverListCmd = &cobra.Command{
+		Use:   "list <projectUID>",
+		Long:  "List servers under a specific project-UID",
+		Short: "List servers under a specific project-UID",
+		RunE:  serverList,
+	}
+	serverPatchCmd = &cobra.Command{
 		Use:    "patch <projectUID>--<name>",
 		Long:   "Patch a server",
 		Short:  "Patch a server",
-		RunE:   patchGWServer,
+		RunE:   serverPatch,
 		PreRun: bindPFlags,
 	}
-	gwServerDeleteCmd = &cobra.Command{
+	serverDeleteCmd = &cobra.Command{
 		Use:   "delete <projectUID>--<name>",
 		Long:  "Delete a server",
 		Short: "Delete a server",
-		RunE:  deleteGWServer,
+		RunE:  serverDelete,
 	}
-	gwServerListCmd = &cobra.Command{
-		Use:   "list <projectUID>",
-		Long:  "List servers",
-		Short: "List servers",
-		RunE:  listGWServers,
-	}
-	// gwClientCmd = &cobra.Command{
+	// clientCmd = &cobra.Command{
 	// 	Use:   "client",
 	// 	Short: "manages gateway clients",
 	// 	Long:  "Manage gateway clients which constitute half of gateway pairs",
@@ -79,171 +79,84 @@ var (
 	// 	},
 	// 	PreRun: bindPFlags,
 	// }
-	gwClientCmd = &cobra.Command{
+	clientCmd = &cobra.Command{
 		Use:   "client",
 		Short: "Manage clients",
 	}
-	gwClientCreateCmd = &cobra.Command{
+	clientCreateCmd = &cobra.Command{
 		Use:    "create <projectUID>--<name>",
 		Long:   "Create a client",
 		Short:  "Create a client",
-		RunE:   createGWClient,
+		RunE:   clientCreate,
 		PreRun: bindPFlags,
 	}
-	gwClientDescribeCmd = &cobra.Command{
+	clientDescribeCmd = &cobra.Command{
 		Use:    "describe <projectUID>--<name>",
 		Short:  "Describe a gateway",
 		Long:   "Describe a gateway",
-		RunE:   describeGWClient,
+		RunE:   clientDescribe,
 		PreRun: bindPFlags,
 	}
-	gwClientPatchCmd = &cobra.Command{
+	clientListCmd = &cobra.Command{
+		Use:   "list <projectUID>",
+		Long:  "List clients under a specific project-UID",
+		Short: "List clients under a specific project-UID",
+		RunE:  clientList,
+	}
+	clientPatchCmd = &cobra.Command{
 		Use:    "patch <projectUID>--<name>",
 		Long:   "Patch a client",
 		Short:  "Patch a client",
-		RunE:   patchGWClient,
+		RunE:   clientPatch,
 		PreRun: bindPFlags,
 	}
-	gwClientDeleteCmd = &cobra.Command{
+	clientDeleteCmd = &cobra.Command{
 		Use:   "delete <projectUID>--<name>",
 		Long:  "Delete a client",
 		Short: "Delete a client",
-		RunE:  deleteGWClient,
+		RunE:  clientDelete,
 	}
-
-	// gwClientDeleteCmd = &cobra.Command{
-	// 	Use:   "delete",
-	// 	Short: "deletes a gateway",
-	// 	Long:  "Delete a gateway",
-	// 	RunE: func(cmd *cobra.Command, args []string) error {
-	// 		return gatewayDelete()
-	// 	},
-	// 	PreRun: bindPFlags,
-	// }
 )
 
 func init() {
-	gwCmd.AddCommand(gwServerCmd)
-	gwCmd.AddCommand(gwClientCmd)
+	gwCmd.AddCommand(serverCmd)
+	gwCmd.AddCommand(clientCmd)
 
-	gwServerCmd.AddCommand(gwServerCreateCmd)
-	gwServerCmd.AddCommand(gwServerDescribeCmd)
-	gwServerCmd.AddCommand(gwServerPatchCmd)
-	gwServerCmd.AddCommand(gwServerDeleteCmd)
-	gwServerCmd.AddCommand(gwServerListCmd)
+	serverCmd.AddCommand(serverCreateCmd)
+	serverCmd.AddCommand(serverDescribeCmd)
+	serverCmd.AddCommand(serverListCmd)
+	serverCmd.AddCommand(serverPatchCmd)
+	serverCmd.AddCommand(serverDeleteCmd)
 
-	gwClientCmd.AddCommand(gwClientCreateCmd)
-	gwClientCmd.AddCommand(gwClientDescribeCmd)
-	gwClientCmd.AddCommand(gwClientPatchCmd)
-	gwClientCmd.AddCommand(gwClientDeleteCmd)
+	clientCmd.AddCommand(clientCreateCmd)
+	clientCmd.AddCommand(clientDescribeCmd)
+	clientCmd.AddCommand(clientListCmd)
+	clientCmd.AddCommand(clientPatchCmd)
+	clientCmd.AddCommand(clientDeleteCmd)
 
-	gwServerPatchCmd.Flags().String(serverPatchFlagPipesToAdd, "", "New pipes to add to the gateway server. `Pipes` is a comma-separated-list of pipes (e.g. PIPE_1,PIPE_2). Each pipe has format SVC_NAME_1:CLIENT_POD_PORT_1:REMOTE_SVC_ENDPOINT_1.")
-	if err := gwServerPatchCmd.MarkFlagRequired(serverPatchFlagPipesToAdd); err != nil {
+	serverPatchCmd.Flags().String(serverPatchFlagPipesToAdd, "", "New pipes to add to the gateway server. `Pipes` is a comma-separated-list of pipes (e.g. PIPE_1,PIPE_2). Each pipe has format SVC_NAME_1:CLIENT_POD_PORT_1:REMOTE_SVC_ENDPOINT_1.")
+	if err := serverPatchCmd.MarkFlagRequired(serverPatchFlagPipesToAdd); err != nil {
 		log.Fatal(err)
 	}
 
 	// client create
-	gwClientCreateCmd.Flags().String(clientCreateFlagServer, "", "UID of the peer server of the client")
-	if err := gwClientCreateCmd.MarkFlagRequired(clientCreateFlagServer); err != nil {
+	clientCreateCmd.Flags().String(clientCreateFlagServer, "", "UID of the peer server of the client")
+	if err := clientCreateCmd.MarkFlagRequired(clientCreateFlagServer); err != nil {
 		log.Fatal(err)
 	}
-	gwClientCreateCmd.Flags().String(clientCreateFlagPipes, "", "Comma-separated list of pipe names chosen from the server's `pipes` spec, e.g. `PIPE_NAME_1,PIPE_NAME_2`")
-	if err := gwClientCreateCmd.MarkFlagRequired(clientCreateFlagPipes); err != nil {
+	clientCreateCmd.Flags().String(clientCreateFlagPipes, "", "Comma-separated list of pipe names chosen from the server's `pipes` spec, e.g. `PIPE_NAME_1,PIPE_NAME_2`")
+	if err := clientCreateCmd.MarkFlagRequired(clientCreateFlagPipes); err != nil {
 		log.Fatal(err)
 	}
 
 	// client patch
-	gwClientPatchCmd.Flags().String(clientPatchFlagPipes, "", "Comma-separated list of the new pipe names to add, which are chosen from the server's `pipes` spec, e.g. `NEW_PIPE_NAME_1,NEW_PIPE_NAME_2`")
-	if err := gwClientPatchCmd.MarkFlagRequired(clientPatchFlagPipes); err != nil {
+	clientPatchCmd.Flags().String(clientPatchFlagPipes, "", "Comma-separated list of the new pipe names to add, which are chosen from the server's `pipes` spec, e.g. `NEW_PIPE_NAME_1,NEW_PIPE_NAME_2`")
+	if err := clientPatchCmd.MarkFlagRequired(clientPatchFlagPipes); err != nil {
 		log.Fatal(err)
 	}
-
-	// gwClientCreateCmd.Flags().String("pipes-to-add", "", "New pipes to add to the gateway server. `Pipes` is a comma-separated-list of pipes (e.g. PIPE_1,PIPE_2). Each pipe has format SVC_NAME_1:CLIENT_POD_PORT_1:REMOTE_SVC_ENDPOINT_1.")
-	// if err := gwClientCreateCmd.MarkFlagRequired("pipes-to-add"); err != nil {
-	// 	log.Fatal(err)
-	// }
-	// gwClientCreateCmd.Flags().String("name", "", "Name of the gateway")
-	// gwClientCreateCmd.Flags().String("clients", "", "Name of the gateway clients")
-	// gwClientCreateCmd.Flags().String("project", "", "Project-UID which the gateway belongs to")
-	// gwClientCreateCmd.Flags().String("pipes", "", "Comma-separated-list of pipes (e.g. PIPE_1,PIPE_2). Pipe has format SVC_NAME_1:CLIENT_POD_PORT_1:REMOTE_SVC_ENDPOINT_1.")
-	// if err := gwClientCreateCmd.MarkFlagRequired("name"); err != nil {
-	// 	log.Fatal(err)
-	// }
-	// if err := gwClientCreateCmd.MarkFlagRequired("project"); err != nil {
-	// 	log.Fatal(err)
-	// }
-	// if err := gwClientCreateCmd.MarkFlagRequired("pipes"); err != nil {
-	// 	log.Fatal(err)
-	// }
-	// gwClientCmd.AddCommand(gwClientCreateCmd)
-
-	// gwClientAddPipesCmd.Flags().String("name", "", "Name of the gateway")
-	// gwClientAddPipesCmd.Flags().String("clients", "", "Name of the gateway clients")
-	// gwClientAddPipesCmd.Flags().String("project", "", "Project-UID which the gateway belongs to")
-	// gwClientAddPipesCmd.Flags().String("pipes", "", "Comma-separated-list of pipes (e.g. PIPE_1,PIPE_2). Pipe has format SVC_NAME_1:CLIENT_POD_PORT_1:REMOTE_SVC_ENDPOINT_1.")
-	// if err := gwClientAddPipesCmd.MarkFlagRequired("name"); err != nil {
-	// 	log.Fatal(err)
-	// }
-	// if err := gwClientAddPipesCmd.MarkFlagRequired("project"); err != nil {
-	// 	log.Fatal(err)
-	// }
-	// if err := gwClientAddPipesCmd.MarkFlagRequired("pipes"); err != nil {
-	// 	log.Fatal(err)
-	// }
-	// gwClientCmd.AddCommand(gwClientAddPipesCmd)
-
-	// gwClientDeleteCmd.Flags().String("name", "", "Name of the gateway")
-	// gwClientDeleteCmd.Flags().String("clients", "", "Name of the gateway clients")
-	// gwClientDeleteCmd.Flags().String("project", "", "Project-UID which the gateway belongs to")
-	// if err := gwClientDeleteCmd.MarkFlagRequired("name"); err != nil {
-	// 	log.Fatal(err)
-	// }
-	// if err := gwClientDeleteCmd.MarkFlagRequired("project"); err != nil {
-	// 	log.Fatal(err)
-	// }
-	// gwClientCmd.AddCommand(gwClientDeleteCmd)
-
-	// gwClientDescribeCmd.Flags().String("name", "", "Name of the gateway")
-	// gwClientDescribeCmd.Flags().String("project", "default", "Project-UID which the gateway belongs to")
-	// if err := gwClientDescribeCmd.MarkFlagRequired("name"); err != nil {
-	// 	log.Fatal(err)
-	// }
-	// gwClientCmd.AddCommand(gwClientDescribeCmd)
 }
 
-func createGWServer(cmd *cobra.Command, args []string) error {
-	projectUID, name, err := validateCommandArgsAndParseInstanceID(args)
-	if err != nil {
-		return err
-	}
-	params := gateway.NewPostServerParams()
-	params.SetProjectuid(projectUID)
-
-	req := &models.V1GatewayServerPostRequest{
-		ProjectUID: &projectUID,
-		Name:       &name,
-		// Peers: []*models.V1PeerSpec{{
-		// 	Name:      ptr(""),
-		// 	PublicKey: ptr(""),
-		// }},
-	}
-	// if viper.GetString("pipes") != "" {
-	// 	pipes, err := parseFlagPipes()
-	// 	if err != nil {
-	// 		return fmt.Errorf("failed to parse flag `pipes`: %w", err)
-	// 	}
-	// 	req.Pipes = pipes
-	// }
-	params.SetBody(req)
-
-	resp, err := cloud.Gateway.PostServer(params, nil)
-	if err != nil {
-		return fmt.Errorf("failed to create gateway: %w", err)
-	}
-	return output.YAMLPrinter{}.Print(resp.Payload)
-}
-
-func createGWClient(cmd *cobra.Command, args []string) error {
+func clientCreate(cmd *cobra.Command, args []string) error {
 	projectUID, name, err := validateCommandArgsAndParseInstanceID(args)
 	if err != nil {
 		return err
@@ -281,7 +194,54 @@ func createGWClient(cmd *cobra.Command, args []string) error {
 	return output.YAMLPrinter{}.Print(resp.Payload)
 }
 
-func patchGWClient(cmd *cobra.Command, args []string) error {
+func clientDelete(cmd *cobra.Command, args []string) error {
+	projectUID, name, err := validateCommandArgsAndParseInstanceID(args)
+	if err != nil {
+		return err
+	}
+
+	params := gateway.NewDeleteClientParams()
+	params.SetProjectuid(projectUID)
+	params.SetName(name)
+	resp, err := cloud.Gateway.DeleteClient(params, nil)
+	if err != nil {
+		return fmt.Errorf("deleting gateway client %s/%s: %w", projectUID, name, err)
+	}
+	return output.YAMLPrinter{}.Print(resp.Payload)
+}
+
+func clientDescribe(cmd *cobra.Command, args []string) error {
+	projectUID, name, err := validateCommandArgsAndParseInstanceID(args)
+	if err != nil {
+		return err
+	}
+	params := gateway.NewGetClientParams()
+	params.SetProjectuid(projectUID)
+	params.SetName(name)
+
+	// params.SetBody(&models.V1GatewayDescribeRequest{
+	// 	Name:       ptr(viper.GetString("name")),
+	// 	ProjectUID: ptr(viper.GetString("project")),
+	// })
+
+	resp, err := cloud.Gateway.GetClient(params, nil)
+	if err != nil {
+		return fmt.Errorf("fetching a gateway client: %w", err)
+	}
+	return output.YAMLPrinter{}.Print(resp.Payload)
+}
+
+func clientList(cmd *cobra.Command, args []string) error {
+	params := gateway.NewListClientsParams()
+	params.SetProjectuid(args[0])
+	resp, err := cloud.Gateway.ListClients(params, nil)
+	if err != nil {
+		return fmt.Errorf("failed to list gateway clients: %w", err)
+	}
+	return output.YAMLPrinter{}.Print(resp.Payload)
+}
+
+func clientPatch(cmd *cobra.Command, args []string) error {
 	projectUID, name, err := validateCommandArgsAndParseInstanceID(args)
 	if err != nil {
 		return err
@@ -308,46 +268,39 @@ func patchGWClient(cmd *cobra.Command, args []string) error {
 	return output.YAMLPrinter{}.Print(resp.Payload)
 }
 
-// func gatewayDelete() error {
-// 	params := gateway.NewDeleteParams()
-// 	params.SetBody(&models.V1GatewayDeleteRequest{
-// 		ProjectUID: ptr(viper.GetString("project")),
-// 		Name:       ptr(viper.GetString("name")),
-// 	})
-// 	resp, err := cloud.Gateway.Delete(params, nil)
-// 	if err != nil {
-// 		return fmt.Errorf("failed to delete gateway: %w", err)
-// 	}
-// 	return output.YAMLPrinter{}.Print(resp.Payload)
-// }
+func serverCreate(cmd *cobra.Command, args []string) error {
+	projectUID, name, err := validateCommandArgsAndParseInstanceID(args)
+	if err != nil {
+		return err
+	}
+	params := gateway.NewPostServerParams()
+	params.SetProjectuid(projectUID)
 
-// func gatewayAddPipes() error {
-// 	params := gateway.NewAddPipesParams()
+	req := &models.V1GatewayServerPostRequest{
+		ProjectUID: &projectUID,
+		Name:       &name,
+		// Peers: []*models.V1PeerSpec{{
+		// 	Name:      ptr(""),
+		// 	PublicKey: ptr(""),
+		// }},
+	}
+	// if viper.GetString("pipes") != "" {
+	// 	pipes, err := parseFlagPipes()
+	// 	if err != nil {
+	// 		return fmt.Errorf("failed to parse flag `pipes`: %w", err)
+	// 	}
+	// 	req.Pipes = pipes
+	// }
+	params.SetBody(req)
 
-// 	parsed, err := parseFlagPipes()
-// 	if err != nil {
-// 		return fmt.Errorf("failed to parse flag `pipes`: %w", err)
-// 	}
+	resp, err := cloud.Gateway.PostServer(params, nil)
+	if err != nil {
+		return fmt.Errorf("failed to create gateway: %w", err)
+	}
+	return output.YAMLPrinter{}.Print(resp.Payload)
+}
 
-// 	params.SetBody(&models.V1GatewayAddPipesRequest{
-// 		Name:  ptr(viper.GetString("name")),
-// 		Pipes: parsed,
-// 		Peers: []*models.V1PeerSpec{{
-// 			Name:      ptr(""),
-// 			PublicKey: ptr(""),
-// 		}},
-// 		ProjectUID: ptr(viper.GetString("project")),
-// 		Type:       ptr("client"),
-// 	})
-
-// 	resp, err := cloud.Gateway.AddPipes(params, nil)
-// 	if err != nil {
-// 		return fmt.Errorf("failed to add pipes to the gateway: %w", err)
-// 	}
-// 	return output.YAMLPrinter{}.Print(resp.Payload)
-// }
-
-func deleteGWServer(cmd *cobra.Command, args []string) error {
+func serverDelete(cmd *cobra.Command, args []string) error {
 	projectUID, name, err := validateCommandArgsAndParseInstanceID(args)
 	if err != nil {
 		return err
@@ -363,33 +316,7 @@ func deleteGWServer(cmd *cobra.Command, args []string) error {
 	return output.YAMLPrinter{}.Print(resp.Payload)
 }
 
-func deleteGWClient(cmd *cobra.Command, args []string) error {
-	projectUID, name, err := validateCommandArgsAndParseInstanceID(args)
-	if err != nil {
-		return err
-	}
-
-	params := gateway.NewDeleteClientParams()
-	params.SetProjectuid(projectUID)
-	params.SetName(name)
-	resp, err := cloud.Gateway.DeleteClient(params, nil)
-	if err != nil {
-		return fmt.Errorf("deleting gateway client %s/%s: %w", projectUID, name, err)
-	}
-	return output.YAMLPrinter{}.Print(resp.Payload)
-}
-
-func listGWServers(cmd *cobra.Command, args []string) error {
-	params := gateway.NewListServersParams()
-	params.SetProjectuid(args[0])
-	resp, err := cloud.Gateway.ListServers(params, nil)
-	if err != nil {
-		return fmt.Errorf("failed to list gateway servers: %w", err)
-	}
-	return output.YAMLPrinter{}.Print(resp.Payload)
-}
-
-func describeGWServer(cmd *cobra.Command, args []string) error {
+func serverDescribe(cmd *cobra.Command, args []string) error {
 	projectUID, name, err := validateCommandArgsAndParseInstanceID(args)
 	if err != nil {
 		return err
@@ -404,7 +331,17 @@ func describeGWServer(cmd *cobra.Command, args []string) error {
 	return output.YAMLPrinter{}.Print(resp.Payload)
 }
 
-func patchGWServer(cmd *cobra.Command, args []string) error {
+func serverList(cmd *cobra.Command, args []string) error {
+	params := gateway.NewListServersParams()
+	params.SetProjectuid(args[0])
+	resp, err := cloud.Gateway.ListServers(params, nil)
+	if err != nil {
+		return fmt.Errorf("failed to list gateway servers: %w", err)
+	}
+	return output.YAMLPrinter{}.Print(resp.Payload)
+}
+
+func serverPatch(cmd *cobra.Command, args []string) error {
 	projectUID, name, err := validateCommandArgsAndParseInstanceID(args)
 	if err != nil {
 		return err
@@ -431,39 +368,7 @@ func patchGWServer(cmd *cobra.Command, args []string) error {
 	return output.YAMLPrinter{}.Print(resp.Payload)
 }
 
-func describeGWClient(cmd *cobra.Command, args []string) error {
-	projectUID, name, err := validateCommandArgsAndParseInstanceID(args)
-	if err != nil {
-		return err
-	}
-	params := gateway.NewGetClientParams()
-	params.SetProjectuid(projectUID)
-	params.SetName(name)
-
-	// params.SetBody(&models.V1GatewayDescribeRequest{
-	// 	Name:       ptr(viper.GetString("name")),
-	// 	ProjectUID: ptr(viper.GetString("project")),
-	// })
-
-	resp, err := cloud.Gateway.GetClient(params, nil)
-	if err != nil {
-		return fmt.Errorf("fetching a gateway client: %w", err)
-	}
-	return output.YAMLPrinter{}.Print(resp.Payload)
-}
-
-// func parseFlagPeers() ([]*models.V1PeerSpec, error) {
-// 	ss := strings.Split(viper.GetString("peers"), ",")
-// 	pipes := []*models.V1PipeSpec{}
-// 	for i := range ss {
-// 		pipe, err := parsePipe(ss[i])
-// 		if err != nil {
-// 			return nil, fmt.Errorf("failed to parse flag `pipes` %v: %w", viper.GetString("pipes"), err)
-// 		}
-// 		pipes = append(pipes, pipe)
-// 	}
-// 	return pipes, nil
-// }
+// other helpers
 
 func parseCommaSeparatedString(s string) ([]string, error) {
 	ss := strings.Split(s, ",")
@@ -471,26 +376,6 @@ func parseCommaSeparatedString(s string) ([]string, error) {
 		return nil, fmt.Errorf("failed to parse %s", s)
 	}
 	return ss, nil
-}
-
-func parsePipeSpecs(specs string) ([]*models.V1PipeSpec, error) {
-	ss := strings.Split(specs, ",")
-	pipes := []*models.V1PipeSpec{}
-	for i := range ss {
-		pipe, err := parsePipeSpec(ss[i])
-		if err != nil {
-			return nil, fmt.Errorf("parsing pipe spec %s: %w", ss[i], err)
-		}
-		pipes = append(pipes, pipe)
-	}
-	return pipes, nil
-}
-
-func validateCommandArgsAndParseInstanceID(args []string) (string, string, error) {
-	if len(args) != 1 {
-		return "", "", fmt.Errorf("command args `%s` should only contain one string", args)
-	}
-	return parseInstanceID(args[0])
 }
 
 func parseInstanceID(id string) (string, string, error) {
@@ -518,6 +403,19 @@ func parsePipeSpec(unparsed string) (*models.V1PipeSpec, error) {
 	return pipe, nil
 }
 
+func parsePipeSpecs(specs string) ([]*models.V1PipeSpec, error) {
+	ss := strings.Split(specs, ",")
+	pipes := []*models.V1PipeSpec{}
+	for i := range ss {
+		pipe, err := parsePipeSpec(ss[i])
+		if err != nil {
+			return nil, fmt.Errorf("parsing pipe spec %s: %w", ss[i], err)
+		}
+		pipes = append(pipes, pipe)
+	}
+	return pipes, nil
+}
+
 func ptr(s string) *string {
 	return pointer.StringPtr(s)
 }
@@ -530,4 +428,11 @@ func u16StrToI64Ptr(s string) (*int64, error) {
 	}
 
 	return pointer.Int64Ptr(int64(u16AsU64)), nil
+}
+
+func validateCommandArgsAndParseInstanceID(args []string) (string, string, error) {
+	if len(args) != 1 {
+		return "", "", fmt.Errorf("command args `%s` should only contain one string", args)
+	}
+	return parseInstanceID(args[0])
 }
