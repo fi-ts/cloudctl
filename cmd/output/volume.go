120d798e507bbda6cc0c45f142559e2ff3ab216f
Stefan Majer <stefan.majer@f-i-ts.de>
2021-06-01 08:48:01 +0200

    Better header (#75)

--- cmd/output/volume.go
+++ cmd/output/volume.go
@@ -168,7 +168,7 @@ func VolumeManifest(v models.V1VolumeResponse, name, namespace string) error {
 }
 
 func (p VolumeClusterInfoTablePrinter) Print(data []*models.V1StorageClusterInfo) {
-	p.wideHeader = []string{"Partition", "Version", "Health", "Nodes NA", "Volumes D/NA/RO", "physical installed/managed", "physical Effective/Free/Used", "Logical Total/Used", "estimated logical Free/Logical", "Compression"}
+	p.wideHeader = []string{"Partition", "Version", "Health", "Nodes NA", "Volumes D/NA/RO", "Physical Installed/Managed", "Physical Effective/Free/Used", "Logical Total/Used", "Estimated Total/Free", "Compression"}
 	p.shortHeader = p.wideHeader
 
 	for _, info := range data {
@@ -214,7 +214,7 @@ func (p VolumeClusterInfoTablePrinter) Print(data []*models.V1StorageClusterInfo
 			installedphysicalstorage + "/" + managedphysicalstorage,
 			effectivephysicalstorage + "/" + freephysicalstorage + "/" + physicalusedstorage,
 			logicalstorage + "/" + logicalusedstorage,
-			estimatedfreelogicalstorage + "/" + estimatedlogicalstorage,
+			estimatedlogicalstorage + "/" + estimatedfreelogicalstorage,
 			compressionratio,
 		}
 		short := wide
