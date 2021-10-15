127347b4d1e134060a5ef7a4950f69aa6961318c
Gerrit <Gerrit91@users.noreply.github.com>
2021-10-15 08:42:50 +0200

    Fixes and improvements for health command. (#123)

--- cmd/health.go
+++ cmd/health.go
@@ -1,8 +1,10 @@
 package cmd
 
 import (
+	"errors"
 	"fmt"
 
+	"github.com/fi-ts/cloud-go/api/client/health"
 	"github.com/fi-ts/cloudctl/cmd/output"
 	"github.com/spf13/cobra"
 )
@@ -14,7 +16,13 @@ func newHealthCmd(c *config) *cobra.Command {
 		RunE: func(cmd *cobra.Command, args []string) error {
 			resp, err := c.cloud.Health.Health(nil, nil)
 			if err != nil {
-				return err
+				var r *health.HealthInternalServerError
+				if errors.As(err, &r) {
+					resp = health.NewHealthOK()
+					resp.Payload = r.Payload
+				} else {
+					return err
+				}
 			}
 
 			must(output.New().Print(resp.Payload))
