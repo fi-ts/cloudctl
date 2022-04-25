9dc6822936fa8c437506440b02ef2578f00f462c
Stefan Majer <stefan.majer@f-i-ts.de>
2022-04-25 11:05:51 +0200

    Normal non-admin users always see 0b usage (#167)

--- cmd/output/volume.go
+++ cmd/output/volume.go
@@ -40,8 +40,8 @@ func (p VolumeTablePrinter) Print(data []*models.V1VolumeResponse) {
 			size = humanize.IBytes(uint64(*vol.Size))
 		}
 		usage := ""
-		if vol.Statistics != nil && vol.Statistics.LogicalUsedStorage != nil {
-			usage = humanize.IBytes(uint64(*vol.Statistics.LogicalUsedStorage))
+		if vol.Statistics != nil && vol.Statistics.PhysicalUsedStorage != nil {
+			usage = humanize.IBytes(uint64(*vol.Statistics.PhysicalUsedStorage))
 		}
 		replica := ""
 		if vol.ReplicaCount != nil {
