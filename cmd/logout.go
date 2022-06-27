dd27c3d43312565b52162ad0351122c97b58fca6
Gerrit <info@gerritschwerthelm.de>
2022-06-27 10:14:37 +0200

    Remove override.

--- cmd/logout.go
+++ cmd/logout.go
@@ -8,21 +8,22 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func newLogoutCmd() *cobra.Command {
+func newLogoutCmd(c *config) *cobra.Command {
 	logoutCmd := &cobra.Command{
 		Use:   "logout",
 		Short: "logout user from OIDC SSO session",
 		RunE: func(cmd *cobra.Command, args []string) error {
 			ctx := api.MustDefaultContext()
 
-			err := auth.Logout(auth.Config{
+			err := auth.Logout(&auth.LogoutParams{
 				IssuerURL: ctx.IssuerURL,
+				Logger:    c.log,
 			})
 			if err != nil {
 				return err
 			}
 
-			fmt.Println("OIDC SSO session successfully logged out. Token is not revoked and are valid until expiration.")
+			fmt.Println("OIDC session successfully logged out. Token is not revoked and is valid until expiration.")
 
 			return nil
 		},
