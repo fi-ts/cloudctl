7e9fc42d030ae883da9179054746a5afbe056e36
Stefan Majer <stefan.majer@f-i-ts.de>
2022-11-09 16:09:06 +0100

    connect to the firewall with userspace vpn (#210)

--- cmd/ssh.go
+++ cmd/ssh.go
@@ -1,11 +1,8 @@
 package cmd
 
 import (
-	"bufio"
 	"context"
-	"encoding/json"
 	"fmt"
-	"io"
 	"net"
 	"net/netip"
 	"os"
@@ -13,198 +10,90 @@ import (
 	"time"
 
 	"github.com/avast/retry-go/v4"
-	dockerclient "github.com/docker/docker/client"
+	"github.com/tailscale/golang-x-crypto/ssh"
+
+	"tailscale.com/tsnet"
+
 	"github.com/fi-ts/cloud-go/api/models"
-	"golang.org/x/crypto/ssh"
-	"golang.org/x/net/proxy"
 	"golang.org/x/term"
-
-	"github.com/docker/docker/api/types"
-	"github.com/docker/docker/api/types/container"
-)
-
-const (
-	tailscaleImage = "tailscale/tailscale:v1.32"
 )
 
 func (c *config) firewallSSHViaVPN(firewallID string, privateKey []byte, vpn *models.V1VPN) (err error) {
 	fmt.Printf("accessing firewall through vpn ")
-
-	cli, err := dockerclient.NewClientWithOpts(dockerclient.FromEnv)
+	hostname, err := os.Hostname()
 	if err != nil {
-		return fmt.Errorf("failed to initialize Docker client: %w", err)
+		return err
+	}
+	s := &tsnet.Server{
+		Hostname:   hostname,
+		ControlURL: *vpn.Address,
+		AuthKey:    *vpn.AuthKey,
+	}
+	defer s.Close()
+
+	// now disable logging, maybe altogether later
+	if os.Getenv("DEBUG") == "" {
+		s.Logf = func(format string, args ...any) {}
 	}
 
-	// Deploy tailscaled
+	start := time.Now()
+	lc, err := s.LocalClient()
+	if err != nil {
+		return err
+	}
 	ctx := context.Background()
-	if err := pullImageIfNotExists(ctx, cli, tailscaleImage); err != nil {
-		return fmt.Errorf("failed to pull tailscale image: %w", err)
-	}
 
-	socksProxyPort, err := getFreePort()
-	if err != nil {
-		return err
-	}
-
-	containerConfig := &container.Config{
-		Image: tailscaleImage,
-		Cmd:   []string{"tailscaled", "--tun=userspace-networking", "--no-logs-no-support", fmt.Sprintf("--socks5-server=:%d", socksProxyPort)},
-	}
-	hostConfig := &container.HostConfig{
-		NetworkMode: container.NetworkMode("host"),
-		AutoRemove:  true,
-	}
-	containerName := "tailscaled-" + firewallID
-	resp, err := cli.ContainerCreate(ctx, containerConfig, hostConfig, nil, nil, containerName)
-	if err != nil {
-		return err
-	}
-
-	tailscaledContainerID := resp.ID
-	if err = cli.ContainerStart(ctx, tailscaledContainerID, types.ContainerStartOptions{}); err != nil {
-		return err
-	}
-	defer func() {
-		if e := cli.ContainerStop(ctx, tailscaledContainerID, nil); e != nil {
-			if err != nil {
-				e = fmt.Errorf("%s: %w", e, err)
-			}
-			err = e
-		}
-	}()
-
-	// Exec tailscale up
-	execConfig := types.ExecConfig{
-		Cmd: []string{"tailscale", "up", "--auth-key=" + *vpn.AuthKey, "--login-server=" + *vpn.Address},
-	}
-	execResp, err := cli.ContainerExecCreate(ctx, containerName, execConfig)
-	if err != nil {
-		return fmt.Errorf("failed to create tailscaled exec: %w", err)
-	}
-	if err := cli.ContainerExecStart(ctx, execResp.ID, types.ExecStartCheck{}); err != nil {
-		return fmt.Errorf("failed to start tailscaled exec: %w", err)
-	}
-
-	// Connect to the firewall via SSH
-	firewallVPNAddr, err := c.getFirewallVPNAddr(ctx, cli, containerName, firewallID)
-	if err != nil {
-		return fmt.Errorf("failed to get Firewall VPN address: %w", err)
-	}
-	ip, err := netip.ParseAddr(firewallVPNAddr)
-	if err != nil {
-		return fmt.Errorf("unable to parse firewall vpn address %w", err)
-	}
-	addr := ip.String()
-	if ip.Is6() {
-		addr = fmt.Sprintf("[%s]", ip)
-	}
-
-	err = sshClientOverSOCKS5("metal", addr, privateKey, 22, fmt.Sprintf(":%d", socksProxyPort))
-	if err != nil {
-		return fmt.Errorf("machine console error:%w", err)
-	}
-
-	return nil
-}
-
-// tailscaleStatus and tailscalePeerStatus structs are used to parse VPN IP for the machine
-type tailscaleStatus struct {
-	Peer map[string]*tailscalePeerStatus
-}
-
-type tailscalePeerStatus struct {
-	HostName     string
-	TailscaleIPs []string
-}
-
-func pullImageIfNotExists(ctx context.Context, cli *dockerclient.Client, tag string) error {
-	images, err := cli.ImageList(ctx, types.ImageListOptions{})
-	if err != nil {
-		return fmt.Errorf("failed to list images: %w", err)
-	}
-
-	for _, i := range images {
-		for _, t := range i.RepoTags {
-			if t == tag {
-				return nil
-			}
-		}
-	}
-
-	reader, err := cli.ImagePull(ctx, tag, types.ImagePullOptions{})
-	if err != nil {
-		return fmt.Errorf("failed to pull image: %w", err)
-	}
-
-	if _, err := io.Copy(io.Discard, reader); err != nil {
-		return fmt.Errorf("failed to load image: %w", err)
-	}
-
-	return nil
-}
-
-func (c *config) getFirewallVPNAddr(ctx context.Context, cli *dockerclient.Client, containerName, fwName string) (addr string, err error) {
-	// Wait until Peers info is filled
-	execConfig := types.ExecConfig{
-		Cmd:          []string{"tailscale", "status", "--json"},
-		AttachStdout: true,
-	}
+	var firewallVPNIP netip.Addr
 	err = retry.Do(
 		func() error {
-			execResp, err := cli.ContainerExecCreate(ctx, containerName, execConfig)
+			fmt.Printf(".")
+			status, err := lc.Status(ctx)
 			if err != nil {
-				return fmt.Errorf("failed to create tailscale status exec: %w", err)
-			}
-			resp, err := cli.ContainerExecAttach(ctx, execResp.ID, types.ExecStartCheck{})
-			if err != nil {
-				return fmt.Errorf("failed to attach to tailscale status exec: %w", err)
-			}
-
-			var data string
-			s := bufio.NewScanner(resp.Reader)
-			for s.Scan() {
-				data += s.Text()
-			}
-
-			// Skipping noise at the beginning
-			var i int
-			for _, c := range data {
-				if c == '{' {
-					break
-				}
-				i++
-			}
-			ts := &tailscaleStatus{}
-			if err := json.Unmarshal([]byte(data[i:]), ts); err != nil {
 				return err
 			}
-
-			if ts.Peer != nil {
-				for _, p := range ts.Peer {
-					if strings.HasPrefix(p.HostName, fwName) {
-						addr = p.TailscaleIPs[0]
+			if status.Self.Online {
+				for _, peer := range status.Peer {
+					if strings.HasPrefix(peer.HostName, firewallID) {
+						firewallVPNIP = peer.TailscaleIPs[0]
+						fmt.Printf(" connected to %s (ip %s) took: %s\n", firewallID, firewallVPNIP, time.Since(start))
 						return nil
 					}
 				}
 			}
-			return fmt.Errorf("failed to find IP for specified firewall")
+			return fmt.Errorf("did not get online")
 		},
 		retry.Attempts(50),
 	)
+	if err != nil {
+		return err
+	}
+	// disable logging after successful connect
+	s.Logf = func(format string, args ...any) {}
 
-	return addr, err
+	conn, err := lc.DialTCP(ctx, firewallVPNIP.String(), 22)
+	if err != nil {
+		return err
+	}
+
+	return sshClientWithConn("metal", hostname, privateKey, conn)
 }
 
-func sshClientOverSOCKS5(user, host string, privateKey []byte, port int, proxyAddr string) error {
+// sshClient opens an interactive ssh session to the host on port with user, authenticated by the key.
+func sshClientWithConn(user, host string, privateKey []byte, conn net.Conn) error {
 	sshConfig, err := getSSHConfig(user, privateKey)
 	if err != nil {
 		return fmt.Errorf("failed to create SSH config: %w", err)
 	}
-	sshServerAddress := fmt.Sprintf("%s:%d", host, port)
-	client, err := getProxiedSSHClient(sshServerAddress, proxyAddr, sshConfig)
+
+	sshConn, sshChan, req, err := ssh.NewClientConn(conn, host, sshConfig)
 	if err != nil {
 		return err
 	}
+	client := ssh.NewClient(sshConn, sshChan, req)
+	if err != nil {
+		return err
+	}
+	defer client.Close()
 
 	return createSSHSession(client, nil)
 }
@@ -222,37 +111,6 @@ func sshClient(user, host string, privateKey []byte, port int, env *env) error {
 	}
 
 	return createSSHSession(client, env)
-}
-
-func getProxiedSSHClient(sshServerAddress, proxyAddr string, sshConfig *ssh.ClientConfig) (*ssh.Client, error) {
-	dialer, err := proxy.SOCKS5("tcp", proxyAddr, nil, proxy.Direct)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create a proxy dialer: %w", err)
-	}
-
-	var conn net.Conn
-	err = retry.Do(
-		func() error {
-			fmt.Printf(".")
-			conn, err = dialer.Dial("tcp", sshServerAddress)
-			if err == nil {
-				fmt.Printf("\n")
-				return nil
-			}
-			return err
-		},
-		retry.Attempts(50),
-	)
-	if err != nil {
-		return nil, fmt.Errorf("failed to connect to proxy at address %s: %w", proxyAddr, err)
-	}
-
-	c, chans, reqs, err := ssh.NewClientConn(conn, sshServerAddress, sshConfig)
-	if err != nil {
-		return nil, fmt.Errorf("failed to create ssh connection: %w", err)
-	}
-
-	return ssh.NewClient(c, chans, reqs), nil
 }
 
 type env struct {
@@ -339,15 +197,4 @@ func getSSHConfig(user string, privateKey []byte) (*ssh.ClientConfig, error) {
 		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
 		Timeout:         10 * time.Second,
 	}, nil
-}
-func getFreePort() (port int, err error) {
-	var a *net.TCPAddr
-	if a, err = net.ResolveTCPAddr("tcp", "localhost:0"); err == nil {
-		var l *net.TCPListener
-		if l, err = net.ListenTCP("tcp", a); err == nil {
-			defer l.Close()
-			return l.Addr().(*net.TCPAddr).Port, nil
-		}
-	}
-	return
 }
