1fe563917764d55ff172ea8befc0c912d87d58f0
Gerrit <info@gerritschwerthelm.de>
2023-08-17 15:11:56 +0200

    Warn users that multiple worker groups lead to higher costs.

--- cmd/cluster.go
+++ cmd/cluster.go
@@ -1019,7 +1019,7 @@ func (c *config) updateCluster(args []string) error {
 				}
 			}
 			if worker == nil && !removeworkergroup {
-				fmt.Println("Adding a new worker group to the cluster.")
+				fmt.Println("Adding a new worker group to the cluster. Please note that running multiple worker groups leads to higher basic costs of the cluster!")
 				err = helper.Prompt("Are you sure? (y/n)", "y")
 				if err != nil {
 					return err
