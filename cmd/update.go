c1d0bde86df2db0c20757568be4a58be68ac8d93
Stefan Majer <stefan.majer@f-i-ts.de>
2023-11-30 15:42:45 +0100

    Check if version is still accepted (#270)

--- cmd/update.go
+++ cmd/update.go
@@ -5,7 +5,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-func newUpdateCmd(name string) *cobra.Command {
+func newUpdateCmd(c *config, name string) *cobra.Command {
 	updateCmd := &cobra.Command{
 		Use:   "update",
 		Short: "update the program",
@@ -14,7 +14,11 @@ func newUpdateCmd(name string) *cobra.Command {
 		Use:   "check",
 		Short: "check for update of the program",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			u, err := updater.New("fi-ts", name, name)
+			desired, err := getDesiredVersion(c)
+			if err != nil {
+				return err
+			}
+			u, err := updater.New("fi-ts", name, name, desired)
 			if err != nil {
 				return err
 			}
@@ -25,7 +29,11 @@ func newUpdateCmd(name string) *cobra.Command {
 		Use:   "do",
 		Short: "do the update of the program",
 		RunE: func(cmd *cobra.Command, args []string) error {
-			u, err := updater.New("fi-ts", name, name)
+			desired, err := getDesiredVersion(c)
+			if err != nil {
+				return err
+			}
+			u, err := updater.New("fi-ts", name, name, desired)
 			if err != nil {
 				return err
 			}
@@ -37,4 +45,15 @@ func newUpdateCmd(name string) *cobra.Command {
 	updateCmd.AddCommand(updateDoCmd)
 
 	return updateCmd
+}
+
+func getDesiredVersion(c *config) (*string, error) {
+	resp, err := c.cloud.Version.Info(nil, nil)
+	if err != nil {
+		return nil, err
+	}
+	if resp.Payload != nil && resp.Payload.MinClientVersion != nil {
+		return resp.Payload.MinClientVersion, nil
+	}
+	return nil, nil
 }
