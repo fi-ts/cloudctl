69f08bdac11767284a56e91438c08e6e0bd6c882
Stefan Majer <stefan.majer@f-i-ts.de>
2024-02-01 10:27:16 +0100

    hint to cluster inputs

--- cmd/cluster.go
+++ cmd/cluster.go
@@ -503,8 +503,8 @@ func (c *config) clusterCreate() error {
 			fmt.Printf(`
 WARNING: You are going to create a cluster which has no internet access with the following consequences:
 - pulling images is only possible from private registries you provide, these registries must be resolvable from the public dns and must be secured with a trusted TLS certificate
-- service type loadbalancer can only be created in networks which are not internet facing
-- cluster wide network policies can only be created in certain network ranges (e.g. MPLS) which are not internet facing
+- service type loadbalancer can only be created in networks which are specified in the allowed networks (see cluster inputs)
+- cluster wide network policies can only be created in certain network ranges whichspecified in the allowed networks (see cluster inputs)
 - It is not possible to change this cluster back to %q after creation
 `, models.V1ClusterCreateRequestNetworkAccessTypeBaseline)
 			err := helper.Prompt("Are you sure? (y/n)", "y")
