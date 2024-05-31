2717a23dd4352249bffe32d73d85be4ded0e8749
Gerrit <info@gerritschwerthelm.de>
2024-05-31 11:36:41 +0200

    Fix tests.

--- cmd/version_test.go
+++ cmd/version_test.go
@@ -1,9 +1,8 @@
 package cmd
 
 import (
+	"bytes"
 	"fmt"
-	"io"
-	"os"
 	"runtime"
 	"testing"
 
@@ -18,13 +17,6 @@ import (
 )
 
 func Test_newVersionCmd(t *testing.T) {
-	rescueStdout := os.Stdout
-	r, w, _ := os.Pipe()
-	os.Stdout = w
-	defer func() {
-		os.Stdout = rescueStdout
-	}()
-
 	v.BuildDate = "1.1.1970"
 	v.GitSHA1 = "abcdef"
 	v.Revision = "v0.0.0"
@@ -35,37 +27,31 @@ func Test_newVersionCmd(t *testing.T) {
 		Version: mockVersionService,
 	}
 
+	var out bytes.Buffer
+
 	mockVersionService.On("Info", mock.Anything, mock.Anything).Return(&version.InfoOK{Payload: &models.RestVersion{Name: pointer.Pointer("cloudctl")}}, nil)
 	cfg := &config{
-		cloud: &cloud,
+		cloud:           &cloud,
+		describePrinter: defaultToYAMLPrinter(&out),
 	}
 	cmd := newVersionCmd(cfg)
-	//cmd.SetOut(w)
-	//cmd.SetArgs([]string{"--o", "yaml"})
+
 	err := cmd.Execute()
 	if err != nil {
 		t.Fatal(err)
 	}
 
 	mockVersionService.AssertExpectations(t)
-	err = w.Close()
-	if err != nil {
-		t.Fatal(err)
-	}
 
-	out, err := io.ReadAll(r)
-	if err != nil {
-		t.Fatal(err)
-	}
-
-	expected := fmt.Sprintf(`client: v0.0.0 (abcdef), v0.0.0, 1.1.1970, %s
-server:
-    builddate: null
-    gitsha1: null
-    minclientversion: null
-    name: cloudctl
-    revision: null
-    version: null
+	expected := fmt.Sprintf(`---
+Client: v0.0.0 (abcdef), v0.0.0, 1.1.1970, %s
+Server:
+  builddate: null
+  gitsha1: null
+  min_client_version: null
+  name: cloudctl
+  revision: null
+  version: null
 `, runtime.Version())
-	assert.Equal(t, expected, string(out))
+	assert.Equal(t, expected, string(out.String()))
 }
