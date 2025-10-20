f5b0f3586cfccd58f572d06e4510937ecee44570
Gerrit <Gerrit91@users.noreply.github.com>
2025-10-20 12:24:49 +0200

    Use human duration from k8s.io to show years. (#354)

--- cmd/helper/helper.go
+++ cmd/helper/helper.go
@@ -5,7 +5,6 @@ import (
 	"errors"
 	"fmt"
 	"io"
-	"math"
 	"os"
 	"os/exec"
 	"path/filepath"
@@ -15,44 +14,13 @@ import (
 	"github.com/go-openapi/runtime"
 	"github.com/go-openapi/strfmt"
 	"gopkg.in/yaml.v3"
+	apiduration "k8s.io/apimachinery/pkg/util/duration"
 	k8syaml "sigs.k8s.io/yaml"
 )
 
 // HumanizeDuration format given duration human readable
 func HumanizeDuration(duration time.Duration) string {
-	days := int64(duration.Hours() / 24)
-	hours := int64(math.Mod(duration.Hours(), 24))
-	minutes := int64(math.Mod(duration.Minutes(), 60))
-	seconds := int64(math.Mod(duration.Seconds(), 60))
-
-	chunks := []struct {
-		singularName string
-		amount       int64
-	}{
-		{"d", days},
-		{"h", hours},
-		{"m", minutes},
-		{"s", seconds},
-	}
-
-	parts := []string{}
-
-	for _, chunk := range chunks {
-		switch chunk.amount {
-		case 0:
-			continue
-		default:
-			parts = append(parts, fmt.Sprintf("%d%s", chunk.amount, chunk.singularName))
-		}
-	}
-
-	if len(parts) == 0 {
-		return "0s"
-	}
-	if len(parts) > 2 {
-		parts = parts[:2]
-	}
-	return strings.Join(parts, " ")
+	return apiduration.HumanDuration(duration)
 }
 
 func HumanizeSize(b int64) string {
